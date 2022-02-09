16222ca041ed3e11996a004a88fbd27b0166f0ea
rahul <rahulgite2999@gmail.com>
2022-02-09 17:10:44 +0530

    Initial Commit

---
+++ src/main/java/com/rahulgite/myvscodejavaproject/MyvscodejavaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.rahulgite.myvscodejavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvscodejavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvscodejavaprojectApplication.class, args);
+	}
+
+}
